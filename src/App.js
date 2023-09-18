d3bb81e2fe6a946ac2b1ffe7acadbd04ac0d7b21
YashMishra0101 <yashrkmfedu6927@gmail.com>
2023-09-18 10:51:21 +0530

    Code optimization

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [selectedFilter, setSelectedFilter] = useState("All");
   
-
+  useEffect(()=>{
   async function getData() {
     setIsLoading(true);
     try {
@@ -32,7 +32,7 @@ function App() {
 
   }
 
-  useEffect(()=>{
+ 
     getData()
   },[])
 
@@ -49,29 +49,3 @@ function App() {
 }
 
 export default App;
-
-
-/*
-
-Certainly, Yash! `useEffect` ka istemal API fetching mein isliye hota hai kyunki ye 
-React components ke sahi tarah se load hone aur data fetching ko control karne mein
-madad karta hai.
-
-Imagine kijiye ki aap ek room mein hain aur aapko kisi se kuch maloomat chahiye. `useEffect` 
-ek alarm clock ki tarah hai. Aap alarm set karte hain aur kehte hain, "Alarm jab room khulta hai 
-(component screen par dikhai deta hai), to maloomat lekar aana (API se data fetch karna)." Yeh 3 
-wajah se acha hota hai:
-
-1. **Timing Ka Control**: Aapka alarm sirf tab bajta hai jab room khulta hai, iska matlab jab aapka 
-component pehli baar screen par aata hai. Isse aap data ko sahi samay par le sakte hain.
-
-2. **State Ki Sudhar**: API se data lete waqt aap apne room ke andar kuch tabdil bhi kar sakte hain. 
-Jaise ki aap kuch saman tayyar kar sakte hain ya loading indicator dikh sakte hain (isLoading ki madad se).
-
-3. **Component Ki Organize**: `useEffect` se aap apne data-fetching logic ko component ke bahar nikal
- sakte hain, jisse aapka component saaf aur organized rahta hai.
-
-Isliye, `useEffect` ka istemal API fetching mein isliye hota hai taaki aap data ko sahi samay par aur 
-sahi tarike se le sakein jab aapka component screen par dikhai deta hai.
-
-*/
